2651aeef2c02684561cba1ff1244b811910398a3
Clarissa <rissaclaa@gmail.com>
2024-04-08 18:07:39 +0800

    Dashboard

---
+++ website/dashboard.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, render_template, request, redirect, url_for, session
+
+dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
+
+@dashboard_bp.route("/")
+def dashboard():
+    plants = [
+        {
+            "title": "Anthie",
+            "image": "plant1.jpg",
+            "species": "Anthurium Magnificum", 
+            "health": "Good", 
+            "soil": "Aroid Soil Mix", 
+            "moisture": "Good", 
+            "humidity": "Good", 
+            "temperature": "20°C",
+            "immediate": "NA",
+            "light": "Indirect Bright Exposure",
+            "lastWater": "2",
+            "lastFertilized": "50",
+            "feedback": [
+                "Consider repotting in spring.",
+                "Looks healthy!"
+            ]
+        },
+
+        {
+            "title": "To-ma-to", 
+            "image": "plant2.jpg", 
+            "species": "Tomato", 
+            "health": "Fair", 
+            "soil": "50% Sphagnum, 50% Perlite", 
+            "moisture": "Low", 
+            "humidity": "Good", 
+            "temperature": "23°C",
+            "immediate": "Water plant as soon as possible",
+            "light": "Direct Exposure",
+            "lastWater": "10",
+            "lastFertilized": "47",
+            "feedback": [
+                "Water plant once a week.",
+                "Fertilize plant once every 40-60 days.",
+                "Try using eggshells once every 12 days for optimal growth rate."
+            ]
+        },
+
+        {
+            "title": "Tom", 
+            "image": "plant2.jpg", 
+            "species": "Tomato", 
+            "health": "Good", 
+            "soil": "Aroid Soil Mix", 
+            "moisture": "Good", 
+            "humidity": "Good", 
+            "temperature": "20°C", 
+            "immediate": "NA",
+            "light": "Direct Exposure",
+            "lastWater": "7",
+            "lastFertilized": "20",
+            "feedback": [
+                "Try using eggshells once every 12 days for optimal growth rate."
+            ]
+        },
+    ]
+
+    if "user" not in session:
+        return redirect(url_for("auth.login"))
+
+    if "header" in session:
+        session.pop("header", None)
+
+    return render_template("dashboard/dashboard.html", plants=plants)
